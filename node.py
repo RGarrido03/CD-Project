69c52ecef9563081a9ae6bde15534d1d742b0279
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-05-19 19:34:13 +0100

    Create Node class with solved & validation props

--- node.py
+++ node.py
@@ -5,6 +5,24 @@ from network import run_http_server
 from p2p import run_p2p_server
 
 
+class Node:
+    def __init__(self, http_port: int, p2p_port, address: str, handicap: int):
+        self.http_port = http_port
+        self.p2p_port = p2p_port
+        self.address = address
+        self.handicap = handicap
+        self.solved = 0
+        self.validations = 0
+
+        self.http_thread = threading.Thread(target=run_http_server, args=(http_port,))
+        self.http_thread.start()
+
+        self.p2p_thread = threading.Thread(
+            target=run_p2p_server, args=(p2p_port, address, handicap)
+        )
+        self.p2p_thread.start()
+
+
 def main():
     parser = argparse.ArgumentParser(conflict_handler="resolve")
     parser.add_argument("-p", "--port", help="Node's HTTP Port", type=int, default=8000)
@@ -21,11 +39,7 @@ def main():
     parser.add_argument("-h", "--handicap", help="Handicap", type=int, default=0)
     args = parser.parse_args()
 
-    http_thread = threading.Thread(target=run_http_server, args=(args.port,))
-    http_thread.start()
-
-    p2p_thread = threading.Thread(target=run_p2p_server, args=(args.service, args.address, args.handicap))
-    p2p_thread.start()
+    Node(args.port, args.service, args.address, args.handicap)
 
 
 if __name__ == "__main__":
